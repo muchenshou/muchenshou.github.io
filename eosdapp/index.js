0a302e755d5a092175779377edb8eb1cc8b54aed
songzhentao <songzhentao@bitdesk.com>
2018-12-07 23:27:58 +0800

    dapp

---
+++ eosdapp/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('./key/private.pem', 'utf8');
+var certificate = fs.readFileSync('./key/file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate,requestCert: false,
+    rejectUnauthorized: false};
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+var PORT = 9000;
+var SSLPORT = 9001;
+var console = require('console');
+app.use(express.static('../'))
+//Welcome
+app.get('/', function(req, res) {
+    console.log(req);
+    if(req.protocol === 'https') {
+        res.status(200).send('Welcome to Safety Land!');
+    }
+    else {
+        res.status(200).send('Welcome!');
+    }
+});
+httpServer.listen( PORT, "0.0.0.0", function() {
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+httpsServer.listen(SSLPORT,  "0.0.0.0",function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
+
